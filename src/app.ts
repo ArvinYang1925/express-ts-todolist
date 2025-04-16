ea423eaa772c9f5e1b376be7079a91963718783e
ArvinYang1925 <arvinyang1925@gmail.com>
2025-04-16 23:51:12 +0800

    feat: 🎸 add TodoList API

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,29 @@
 import "reflect-metadata";
 import express from "express";
+import { AppDataSource } from "./config/db";
+import todoRoutes from "./routes/todoRoutes";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use("/todos", todoRoutes); // 加上 Todo 路由
 
 app.get("/", (req, res) => {
   res.send("Hello, MeowTodo Backend!");
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("📦 DB Connected!");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ DB connection failed:", err);
+  });
